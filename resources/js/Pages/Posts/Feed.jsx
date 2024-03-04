5b5d6299a2d33631d187b2f9ecab928becc4a0be
Richard Ford <richardford10@gmail.com>
2024-03-04 11:27:10 -0500

    whitespace

--- resources/js/Pages/Posts/Feed.jsx
+++ resources/js/Pages/Posts/Feed.jsx
@@ -4,9 +4,9 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 export default function Feed({ posts, auth }) {
   return (
     <div>
-            <AuthenticatedLayout user={auth.user}
+      <AuthenticatedLayout user={auth.user}
         header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Post Feed</h2>}>
-      <Posts posts={posts} />
+        <Posts posts={posts} />
       </AuthenticatedLayout>
     </div>
   );
--- resources/js/Pages/Posts/Post.jsx
+++ resources/js/Pages/Posts/Post.jsx
@@ -3,20 +3,20 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 function Post({ post, auth }) {
     return (
         <AuthenticatedLayout header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Post</h2>} user={auth.user}>
-        <div className="py-12">
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <header>
-                        <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">{post.title}</h2>
-                    </header>
-                    <div className="mt-4 p-4 bg-gray-50 dark:bg-gray-700 rounded-lg">
-                        <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            {post.body}
-                        </p>
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                        <header>
+                            <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">{post.title}</h2>
+                        </header>
+                        <div className="mt-4 p-4 bg-gray-50 dark:bg-gray-700 rounded-lg">
+                            <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                {post.body}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </AuthenticatedLayout>
     );
 }
